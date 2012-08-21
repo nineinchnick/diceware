5c9ebc46271d28b978ef7290ab9de31b8392ae19
Joe Martin <jmartin@desertflood.com>
2012-08-21 08:25:21 -0500

    Add disclaimer to shared footer

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
 <div>
+<p>You probably shouldn't use this code to generate actual pass phrases. How much do you trust the author of a public website? How much do you trust the randomness of RANDOM.ORG? How much do you trust the connection between my server and RANDOM.ORG, to ensure that someone else isn't substituting numbers for the dice rolls? How much do you trust me, to not track the generated numbers and passwords? How much do you trust <a href="http://www.joyent.com/">Joyent</a> (my hosting provider) or all of the networks located between me and you?</p>
+<p>With that much implied trust, you probably shouldn't trust the results of these scripts. But you can if you want.</p>
 <p>
 <a href="index.php">Passphrase</a> | <a href="pin.php">PIN</a> | <a href="passgen25.php">passgen25</a>
 </p>
